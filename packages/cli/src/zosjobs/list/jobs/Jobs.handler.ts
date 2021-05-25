9bc018e66b86f0579f4cfc5abaa1961fd1f00ade
zFernand0 <fernando.rijocedeno@broadcom.com>
2021-05-25 09:07:28 -0400

    recover some updates for quick demo :cry:
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

--- packages/cli/src/zosjobs/list/jobs/Jobs.handler.ts
+++ packages/cli/src/zosjobs/list/jobs/Jobs.handler.ts
@@ -10,7 +10,7 @@
 */
 
 import { IHandlerParameters, ICommandOutputFormat } from "@zowe/imperative";
-import { IJob, GetJobs, JobsConstants } from "@zowe/zos-jobs-for-zowe-sdk";
+import { IJob, GetJobs, JobsConstants, IJobFile } from "@zowe/zos-jobs-for-zowe-sdk";
 import { ZosmfBaseHandler } from "@zowe/zosmf-for-zowe-sdk";
 
 /**
@@ -44,10 +44,41 @@ export default class JobsHandler extends ZosmfBaseHandler {
             // let temp: any = params.response.format.output(format, true);
             // console.log("temp: ", temp);
             let temp: string = params.response.format.formatOutput({...format, ...{header: true}}, null, true);
-            const lines = temp.split("\n");
-            const header = " " + lines[0];
+            let lines = temp.split("\n");
+            let header = " " + lines[0];
             lines.shift();
-            params.response.console.interactiveSelection(lines, {header});
+            let selected = await params.response.console.interactiveSelection(lines, {header});
+
+            if (!params.arguments.daemon) params.response.console.log("");
+
+            const spoolfileid = lines[selected-1].split(" ")[0];
+            const job: IJob = await GetJobs.getJob(this.mSession, spoolfileid);
+            const files: IJobFile[] = await GetJobs.getSpoolFilesForJob(this.mSession, job);
+
+            // Set the object, message, and log the prettified object
+            params.response.data.setObj(files);
+            params.response.data.setMessage(`"${files.length}" spool files obtained for job "${job.jobname}(${job.jobid})"`);
+
+            const jobFormat: ICommandOutputFormat = {
+                fields: ["id", "ddname", "procstep", "stepname"],
+                output: files,
+                format: "table"
+            };
+
+            temp = params.response.format.formatOutput({...jobFormat, ...{header: true}}, null, true);
+            lines = temp.split("\n");
+            header = " " + lines[0];
+            lines.shift();
+            selected = await params.response.console.interactiveSelection(lines, {header});
+
+            if (!params.arguments.daemon) params.response.console.log("");
+
+            const spoolfileidSelected = +(lines[selected-1].split(" ")[0]);
+            const content: string = await GetJobs.getSpoolContentById(this.mSession, job.jobname, job.jobid, spoolfileidSelected);
+            params.response.data.setObj(content);
+            params.response.data.setMessage(`Spool file "${spoolfileid}" content obtained for job "${job.jobname}(${job.jobid})"`);
+            params.response.console.log(Buffer.from(content));
+
         } else {
             // Populate the response object
             params.response.data.setObj(jobs);
